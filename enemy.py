672cc4e4ab76ada08af2d749afdfc87ae6874ed6
Cyrus Tabatabai-Yazdi <24766642+cyrustabatab@users.noreply.github.com>
2021-01-29 23:05:45 -0800

    add random enemy movement(chooses direction randomly)

--- enemy.py
+++ enemy.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,random
 from settings import *
 
 vec = pygame.math.Vector2
@@ -13,7 +13,7 @@ class Enemy:
         self.radius = self.game.cell_width//2.3
         self.number = number
         self.set_color()
-        self.direction = vec(0,0)
+        self.direction = vec(1,0)
         self.set_personality()
 
 
@@ -22,18 +22,50 @@ class Enemy:
 
     def update(self):
         self.pixel_pos += self.direction
-        if self.time_to_move:
+        if self.time_to_move():
             self.move()
 
+        self.grid_pos[0],self.grid_pos[1] = (self.pixel_pos.x-TOP_BOTTOM_GAP//2)//self.game.cell_width,(self.pixel_pos.y - TOP_BOTTOM_GAP)//self.game.cell_height + 1
 
     def draw(self):
         pygame.draw.circle(self.game.screen,self.color,(int(self.pixel_pos.x),int(self.pixel_pos.y)),self.radius)
     
     def time_to_move(self):
-        pass
+        if int(self.pixel_pos.x - self.game.cell_width//2 - 5) % self.game.cell_width == 0:
+            if self.direction == vec(1,0) or self.direction == vec(-1,0):
+                return True
+        if int(self.pixel_pos.y - self.game.cell_height//2 - 5) % self.game.cell_height == 0:
+            if self.direction == vec(0,1) or self.direction == vec(0,-1):
+                return True
+
+
+        return False
+
 
     def move(self):
-        pass
+        if self.personality == 'random':
+            self.direction = self.get_random_direction()
+    
+
+
+    def get_random_direction(self):
+        while True:
+            number = random.random()
+            if number < 0.25:
+                x_dir,y_dir = 1,0
+            elif number >= 0.25 and number < 0.5:
+                x_dir,y_dir = 0,1
+            elif number >= 0.5 and number < 0.75:
+                x_dir,y_dir = -1,0
+            else:
+                x_dir,y_dir = 0,-1
+            
+            direction = vec(x_dir,y_dir)
+            if self.grid_pos + direction not in self.game.walls:
+                break
+
+
+        return direction
 
     def set_color(self):
         if self.number == 0:
